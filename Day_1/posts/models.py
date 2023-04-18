671e6de39118e3cdcd83a8823081c9929cae6470
Abdelrahman-95 <abdomoh.7e4@gmail.com>
2023-04-18 17:42:01 +0200

    day 1 create project and app

---
+++ Day_1/posts/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class Post (models.Model):
+    Title   = models.CharField(max_length=100)
+    Content = models.TextField(max_length=5000)
+    Image   = models.ImageField(upload_to='posts/')
+
+    def __str__(self):
+        return self.Title
